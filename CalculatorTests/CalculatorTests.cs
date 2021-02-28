c3aeac228485fc1070eb5d064c7c965b79ca50fd
Sam Skinner <sam.sjskinner@gmail.com>
2021-03-01 10:47:29 +1100

    Step 3: Calculator can take comma seperated values and sum

--- CalculatorTests/CalculatorTests.cs
+++ CalculatorTests/CalculatorTests.cs
@@ -13,7 +13,7 @@ namespace CalculatorTests
             string input = "";
 
             // Act
-            int output = Calculator.Calculate(input);
+            int output = Calculator.Add(input);
 
             // Assert
             Assert.IsType<Int32>(output);
@@ -27,8 +27,22 @@ namespace CalculatorTests
             int expectedOutput = 5;
 
             // Act
-            int output = Calculator.Calculate(input);
+            int output = Calculator.Add(input);
 
+            // Assert
+            Assert.Equal(expectedOutput, output);
+        }
+
+        [Fact]
+        public void Test_Can_Sum_Two_Numbers()
+        {
+            // Arrange
+            string input = "3,5";
+            int expectedOutput = 8;
+            
+            // Act
+            int output = Calculator.Add(input);
+            
             // Assert
             Assert.Equal(expectedOutput, output);
         }
--- StringCalculator/Calculator.cs
+++ StringCalculator/Calculator.cs
@@ -1,13 +1,22 @@
 using System;
+using System.Linq;
 
 namespace StringCalculator
 {
     public static class Calculator
     {
-        public static int Calculate(string toConvert)
+        public static int Add(string toConvert)
         {
             if (toConvert == "") return 0; // This seems like a poor solution but works for now
-            return Int32.Parse(toConvert);
+
+            string[] stringsToInt = toConvert.Split(",");
+            int[] toSum = new int[stringsToInt.Length];
+            for (int i = 0; i < stringsToInt.Length; i++)
+            {
+                toSum[i] = Int32.Parse(stringsToInt[i]);
+            }
+
+            return toSum.Sum();
         }
     }
 }
